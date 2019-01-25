b2d689c2649bfeeb8441e5b8ea93da95ba60f2dc
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-26 00:04:20 +0530

    day q2

--- app.s
+++ app.s
@@ -4,21 +4,21 @@
 ;ARM CODE 
 .code 32		;word align
 .text	
-.global main_asm
+.global add
 
 ;/* Application code for assembly starts here */
 
-main_asm:
-	mov r0,#2
-	mov r1,#3
-	mov r2,#4
-	mov r3,#1
-	mov r4,#5
-	mov r5,#5
+
+add:	
+	add r0,r0,r1
+	add r0,r0,r2
+	add r0,r0,r3
+	
 	stmfd sp!,{r4-r5,lr}
-	bl add
-	mov r6,r0 
+	add r0,r0,r4
+	add r0,r0,r5
+	bx lr			
 	
 	
-loop:   b loop
+
 .end 
